4d9489890a58c919c0fd449947698ce7415974a0
Erik Martín Garzón <erikmarting1200@gmail.com>
2023-06-19 13:12:40 +0200

    Numbers drop UI

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -7,11 +7,24 @@ public class UiManager : MonoBehaviour
 {
     public TMP_Text yearsText;
     public float years;
+    public float actualYears;
     public GameObject microphone;
     public GameObject guitar;
     private DetectorEchoAttack detectorEchoAttack;
     public int levelInd;
+    public int CountFPS = 30;
+    public float duration = 1f;
+    private Coroutine CountingCoroutine;
 
+    private void UpdateText(int newValue)
+    {
+        if (CountingCoroutine != null)
+        {
+            StopCoroutine(CountingCoroutine);
+        }
+        CountingCoroutine = StartCoroutine(CountText(newValue));
+
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -28,27 +41,27 @@ public class UiManager : MonoBehaviour
         switch(levelInd)
         {
             case 1:
-                years = 3000f;
+                UpdateText(3000);
                 break;
 
             case 2:
-                years = 1600f;
+                UpdateText(1600);
                 break;
 
             case 3:
-                years = 500f;
+                UpdateText(500);
                 break;
 
             case 4:
-                years = 1600f;
+                UpdateText(1600);
                 break;
 
             case 5:
-                years = 3000f;
+                UpdateText(3000);
                 break;
 
         }
-        yearsText.text = years.ToString() + " A.C";
+        
 
         if (detectorEchoAttack.echoReady)
             guitar.SetActive(true);
@@ -57,5 +70,49 @@ public class UiManager : MonoBehaviour
 
         
 
+    }
+    
+    private IEnumerator CountText(int newValue)
+    {
+        WaitForSeconds Wait = new WaitForSeconds(1f / CountFPS) ;
+        float previousYears = years;
+        float stepAmount;
+
+        if (newValue - previousYears < 0)
+        {
+            stepAmount = Mathf.FloorToInt((newValue - previousYears) / (CountFPS * duration)); // newValue = -20, previousValue = 0. CountFPS = 30, and Duration = 1; (-20- 0) / (30*1) // -0.66667 (ceiltoint)-> 0
+        }
+        else
+        {
+            stepAmount = Mathf.CeilToInt((newValue - previousYears) / (CountFPS * duration)); // newValue = -20, previousValue = 0. CountFPS = 30, and Duration = 1; (-20- 0) / (30*1) // -0.66667 (ceiltoint)-> 0
+        }
+
+        if (previousYears < newValue)
+        {
+            while(previousYears < newValue)
+            {
+                previousYears += stepAmount;
+                if (previousYears > newValue)
+                {
+                    previousYears = newValue;
+                }
+                yearsText.text = previousYears.ToString() + " A.C";
+                yield return Wait;
+            }
+        }
+        else
+        {
+            while (previousYears > newValue)
+            {
+                previousYears += stepAmount;
+                if (previousYears < newValue)
+                {
+                    previousYears = newValue;
+                }
+                yearsText.text = previousYears.ToString() + " A.C";
+                yield return Wait;
+            }
+        }
+       
     }
 }
