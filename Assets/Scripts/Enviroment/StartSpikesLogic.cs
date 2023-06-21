b301d5912ce9c829eb45f161e0af79f4971d3007
VoZeS <davidboces12@hotmail.com>
2023-06-21 08:52:43 +0200

    TORCH SPIKES DONE

--- Assets/Scripts/Enviroment/StartSpikesLogic.cs
+++ Assets/Scripts/Enviroment/StartSpikesLogic.cs
@@ -6,10 +6,20 @@ public class StartSpikesLogic : MonoBehaviour
 {
     public bool start;
 
+    public ParticleSystem chispas1;
+    public ParticleSystem chispas2;
+    public ParticleSystem chispas3;
+    public ParticleSystem chispas4;
+
     // Start is called before the first frame update
     void Start()
     {
         start = false;
+
+        chispas1.Pause();
+        chispas2.Pause();
+        chispas3.Pause();
+        chispas4.Pause();
     }
 
     // Update is called once per frame
@@ -21,6 +31,13 @@ public class StartSpikesLogic : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
+        {
             start = true;
+
+            chispas1.Play();
+            chispas2.Play();
+            chispas3.Play();
+            chispas4.Play();
+        }
     }
 }
